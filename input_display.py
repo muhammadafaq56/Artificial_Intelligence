9003c3e8b3fe712275306ef7d317e8cd973e0962
muhammadafaq56 <afaqkh22112006@gmail.com>
2025-03-13 00:03:03 +0500

    Take input from user and display it on console

---
+++ input_display.py
@@ -0,0 +1,11 @@
+# 1. Input and display a string  
+string = input("Enter some text: ")  
+print("You entered:", string)  
+
+# 2. Input and display an integer  
+integer = int(input("Enter an integer: "))  
+print("You entered the number:", integer)  
+
+# 3. Input and display a float  
+floating = float(input("Enter a decimal number: "))  
+print("You entered the float:", floating)  
