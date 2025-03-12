60ad5409ff51ea64a4cee39ea2b6cd8603c17cc9
muhammadafaq56 <afaqkh22112006@gmail.com>
2025-03-13 00:23:16 +0500

    Display the types of variables

---
+++ type_casting.py
@@ -0,0 +1,38 @@
+# DATA TYPES AND TYPE CASTING  
+# To check the type of any variable, we use the type() function in Python.  
+# Type represents the kind of value and determines how the value can be used.  
+
+print('\nFrom here we will show variables and their types:')  
+
+# TYPE 1: NUMBERS (include integers, floating point values, complex numbers)  
+var1 = -25  
+print(var1)  
+print(type(var1))  
+
+var2 = 500  
+print(var2)  
+print(type(var2))  
+
+var3 = 3.14  
+print(var3)  
+print(type(var3))  
+
+var4 = 4E5  # 4 * 10^5  
+print(var4)  
+print(type(var4))  
+
+var5 = 2.5e-3  # 2.5 * 10^-3 or 0.0025  
+print(var5)  
+print(type(var5))  
+
+var6 = complex(3, -7)  
+print(var6)  
+print(type(var6))  
+
+var7 = 2 - 5j  
+print(var7)  
+print(type(var7))  
+
+var8 = 4 + 6J  
+print(var8)  
+print(type(var8))  
