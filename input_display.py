501d5609686370a93e7c27853bcc073d13859248
muhammadafaq56 <afaqkh22112006@gmail.com>
2025-03-13 00:13:50 +0500

    Take input from user and display it on the console

---
+++ input_display.py
@@ -0,0 +1,16 @@
+# 1. Input and display a string  
+string = input("Enter some text: ")  
+print("You entered:", string)  
+
+# 2. Input and display an integer  
+integer = int(input("Enter an integer: "))  
+print("You entered the number:", integer)  
+
+# 3. Input and display a float  
+floating = float(input("Enter a decimal number: "))  
+print("You entered the float:", floating)  
+
+# WE CAN PRINT 2 STATEMENTS IN A SINGLE LINE USING SEMICOLON (;)
+statement1 = "It's the first statement"
+statement2 = "It's the second statement"
+print(statement1); print(statement2)
