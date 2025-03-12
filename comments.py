7ef3c6c49802b19eaf786902d16d9a3d8667a214
muhammadafaq56 <afaqkh22112006@gmail.com>
2025-03-12 23:38:02 +0500

    This file includes the basic definition of comment

---
+++ comments.py
@@ -0,0 +1,4 @@
+#In Python, a comment starts with a hash symbol (#) and extends to the end of the line. The interpreter ignores everything after #. Comments help explain code but do not affect execution. Python does not support multi-line or block comments
+# Here is a code of pyton, which is ignored because # is used
+# print("Hello, World!")  
+# This part is ignored
