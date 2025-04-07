fb7ad84d1be9efb36ba098908623f172e441b491
muhammadafaq56 <afaqkh22112006@gmail.com>
2025-04-07 17:25:26 +0500

    Skip 3 and 6 using continue in FOR loop

---
+++ lab 03_practice/08.py
@@ -0,0 +1,7 @@
+# Iterate through numbers from 0 to 6
+for i in range(0, 7):
+    # If i is 3 or 6, skip this iteration using continue
+    if(i == 3 or i == 6):
+        continue
+    # Print the value of i for the remaining iterations
+    print(i)
